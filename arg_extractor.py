c59f38441ca9f1542077b53abb0d3cfc9cfc1c92
JorgeIvanJH <jorgeivan.jaramillo.herrera@gmail.com>
2025-05-13 22:05:39 +0100

    models and args added

--- arg_extractor.py
+++ arg_extractor.py
@@ -29,8 +29,9 @@ def parse_args():
     # General
     parser.add_argument('--config_path', type=str, default="config.yaml", help='Path to YAML config file')
     parser.add_argument('--experiment_name', type=str, default="exp_1", help='Name of the experiment')
-    parser.add_argument('--model_name', type=str, choices=AVAILABLE_MODELS, default=AVAILABLE_MODELS[0], help='Name of the model to train')
-
+    parser.add_argument('--model_name', type=str,
+                        help=f'Name of the model to use for training in MODELS_PATH in config.yaml')
+    
     # Training
     parser.add_argument('--batch_size', type=int, help='Batch size for training')
     parser.add_argument('--lr', type=float, help='Learning rate')
@@ -40,9 +41,11 @@ def parse_args():
     parser.add_argument('--load_encoder_weights', type=str2bool, default=False, help='Load pretrained encoder weights')
 
     # System
-    parser.add_argument('--use_gpu', type=str2bool, help='Whether to use GPU')
     parser.add_argument('--num_workers', type=int, help='Dataloader workers')
 
+    # Model
+
+    
     # Other
     parser.add_argument('--seed', type=int, help='Random seed')
 
@@ -61,17 +64,21 @@ def merge_configs(yaml_config, cli_args):
         "NUM_POINTS_PER_SEG_SAMPLE": yaml_config["NUM_POINTS_PER_SEG_SAMPLE"],
         "DEVICE": yaml_config.get("DEVICE", "cpu"),
         "EXPERIMENT_NAME": cli_args.experiment_name,
-        "MODEL_NAME": cli_args.model_name,
+        "MODEL_NAME": cli_args.model_name.lower() if cli_args.model_name is not None else train_config.get("NAME", "testmodel").lower(),
         "BATCH_SIZE": cli_args.batch_size if cli_args.batch_size is not None else train_config.get("BATCH_SIZE", 32),
         "LR": cli_args.lr if cli_args.lr is not None else train_config.get("LR", 0.001),
         "EPOCHS": cli_args.num_epochs if cli_args.num_epochs is not None else train_config.get("EPOCHS", 100),
-        "WEIGHT_DECAY": cli_args.weight_decay_coefficient if cli_args.weight_decay_coefficient is not None else 0.0,
+        "WEIGHT_DECAY": cli_args.weight_decay_coefficient if cli_args.weight_decay_coefficient is not None else train_config.get("WEIGHT_DECAY", 0),
         "CONTINUE_FROM_EPOCH": cli_args.continue_from_epoch,
         "LOAD_ENCODER_WEIGHTS": cli_args.load_encoder_weights,
-        "USE_GPU": cli_args.use_gpu if cli_args.use_gpu is not None else (yaml_config.get("DEVICE") == "cuda"),
         "NUM_WORKERS": cli_args.num_workers if cli_args.num_workers is not None else train_config.get("NUM_WORKERS", 4),
         "SHUFFLE": train_config.get("SHUFFLE", True)
     }
+
+    available_models = [f.replace(".py","") for f in os.listdir(merged_config["MODELS_PATH"]) if f.endswith('.py')]
+    print(f"Available models: {available_models}")
+    assert merged_config["MODEL_NAME"] in available_models, f"Model {merged_config['MODEL_NAME']} not found in {merged_config['MODELS_PATH']}. Available models: {available_models}"
+    
     return merged_config
 
 
--- train.py
+++ train.py
@@ -1,27 +1,18 @@
-model = "PointNet"
-
+import os
 import numpy as np
-
-import h5py
-import yaml
-from data.data_provider import SegmentationDataset
 import torch
-from torch.utils.data import DataLoader
 import torch.optim as optim
 from torchmetrics.classification import MulticlassMatthewsCorrCoef
-
-import matplotlib.pyplot as plt
-
-from models.item_pointnet2_torch import PointNet2_SegHead
-from models.item_pointnet2_torch import PointNet2_SegLoss
-
+from experiment_builder import ExperimentBuilder
 from data.data_provider import get_data_loaders
 
 from arg_extractor import get_config
 
 c = get_config()
 print(c)
+DEVICE = torch.device("cuda" if (torch.cuda.is_available() and c["DEVICE"] == "cuda" ) else "cpu")
 
+# Data Providers
 train_dataloader, valid_dataloader = get_data_loaders(
     object_segmentation_dataset_directory=c["DATA_PATH"],
     target_object_dataset_name=c["OBJECT_NAME"],
@@ -31,21 +22,52 @@ train_dataloader, valid_dataloader = get_data_loaders(
     seed=c["SEED"]
 )
 
-
-# points, targets = next(iter(train_dataloader))
-# NUM_CLASSES = targets.unique().shape[0]
-# seg_model = PointNet2_SegHead(num_points=NUM_POINTS_PER_SEG_SAMPLE, m=NUM_CLASSES)
-
-
-# alpha = np.ones(NUM_CLASSES)
-# gamma = 1
-# optimizer = optim.Adam(seg_model.parameters(), lr=LR)
-# num_iterations_in_epoch = len(train_dataloader)
-# step_size = int(num_iterations_in_epoch * 8)     # as recommended by the paper \ref{smith2017cyclical}
-# scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=LR, max_lr=0.01, step_size_up=step_size, cycle_momentum=False) # cycle_momentum=False to be compatible with Adam
-# criterion = PointNet2_SegLoss(alpha=alpha, gamma=gamma, dice=True).to(DEVICE)
-# seg_model = seg_model.to(DEVICE)
-# mcc_metric = MulticlassMatthewsCorrCoef(num_classes=NUM_CLASSES).to(DEVICE)
+# Model
+points, targets = next(iter(train_dataloader))
+NUM_CLASSES = targets.unique().shape[0]
+if c["MODEL_NAME"] == "pointnet2":
+    print(f"Using PointNet2 model for segmentation on {c['OBJECT_NAME']}")
+    from models.pointnet2 import PointNet2_SegHead
+    from models.pointnet2 import PointNet2_SegLoss
+    seg_model = PointNet2_SegHead(num_points=c["NUM_POINTS_PER_SEG_SAMPLE"], m=NUM_CLASSES)
+    seg_model = seg_model.to(DEVICE)
+    alpha = np.ones(NUM_CLASSES)
+    gamma = 1
+    criterion = PointNet2_SegLoss(alpha=alpha, gamma=gamma, dice=True).to(DEVICE)
+    # Training parameters
+    optimizer = optim.Adam(seg_model.parameters(), lr=c["LR"])
+    num_iterations_in_epoch = len(train_dataloader)
+    step_size = int(num_iterations_in_epoch * 8)     # as recommended by the paper \ref{smith2017cyclical}
+    scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=c["LR"], max_lr=0.01, step_size_up=step_size, cycle_momentum=False) # cycle_momentum=False to be compatible with Adam
+    mcc_metric = MulticlassMatthewsCorrCoef(num_classes=NUM_CLASSES).to(DEVICE)
+elif c["MODEL_NAME"] == "pointnet":
+    print(f"Using PointNet model for segmentation on {c['OBJECT_NAME']}")
+    from models.pointnet import PointNetSegHead
+    from models.pointnet import PointNetSegLoss
+    seg_model = PointNetSegHead(num_points=c["NUM_POINTS_PER_SEG_SAMPLE"], m=NUM_CLASSES)
+    seg_model = seg_model.to(DEVICE)
+    alpha = np.ones(NUM_CLASSES)
+    gamma = 1
+    criterion = PointNetSegLoss(alpha=alpha, gamma=gamma, dice=True).to(DEVICE)
+    # Training parameters
+    optimizer = optim.Adam(seg_model.parameters(), lr=c["LR"])
+    num_iterations_in_epoch = len(train_dataloader)
+    step_size = int(num_iterations_in_epoch * 8)     # as recommended by the paper \ref{smith2017cyclical}
+    scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=c["LR"], max_lr=0.01, step_size_up=step_size, cycle_momentum=False)
+    mcc_metric = MulticlassMatthewsCorrCoef(num_classes=NUM_CLASSES).to(DEVICE)
 
 
 # # TRAINING LOOP AND VALIDATION LOOP SAVE
+
+conv_experiment = ExperimentBuilder(network_model=seg_model,
+                                    experiment_name=c["EXPERIMENT_NAME"] + "_" + c["MODEL_NAME"],
+                                    num_epochs=c["EPOCHS"],
+                                    weight_decay_coefficient=c["WEIGHT_DECAY"],
+                                    device=DEVICE,
+                                    continue_from_epoch=c["CONTINUE_FROM_EPOCH"],
+                                    train_data=train_dataloader, 
+                                    val_data=valid_dataloader,
+                                    test_data=valid_dataloader, # TODO: change to test data
+                                    lr = c["LR"],
+                                    )  # build an experiment object
+experiment_metrics, test_metrics = conv_experiment.run_experiment()  # run experiment and return experiment metrics
